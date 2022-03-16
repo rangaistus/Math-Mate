c639b2f605a36b3337fe538d9a09c8cc362c5a6e
rangaistus <6002029@mborijnland.nl>
2022-03-16 10:38:18 +0100

    Update even-of-oneven.js

--- js/even-of-oneven.js
+++ js/even-of-oneven.js
@@ -1,8 +1,5 @@
 function evenOfOneven() {
-
-
     let getal1 = document.getElementById('getal1').value;
-    
     let uitkomst = Number.isInteger(getal1 / 2)
 
     document.getElementById('uitkomst').innerHTML = uitkomst
