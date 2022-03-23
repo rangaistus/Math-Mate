77c5c31e14fdffe0a63d5d819582afdd250308e9
yarnickdepanda <yarnick153@gmail.com>
2022-03-23 10:55:38 +0100

    breuken werken

--- js/breuken.js
+++ js/breuken.js
@@ -2,14 +2,15 @@ function breuken() {
 
     let input1 = document.getElementById('getal1').value;
     let getal1 = parseFloat(input1)
-    let uitkomst;
-    document.getElementById('uitkomst').innerHTML = "";
+    document.getElementById('uitkomst').innerHTML = ""
 
-    let counter = 0;
+    let counter = 1;
 
+    while (counter <= getal1) {
+        document.getElementById('uitkomst').innerHTML += " 1/" + counter + " = " + 1 / counter + "<br>";
+        counter++;
 
-
-
-    while (counter < 7) {
-        (counter++)
     }
+}
+
+document.getElementById('bereken').addEventListener("click", breuken);
