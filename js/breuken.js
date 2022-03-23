755be9e0f9479324181a190316c77600528c9c24
yarnickdepanda <yarnick153@gmail.com>
2022-03-23 10:37:26 +0100

    machten/breuken toegevoegd

---
+++ js/breuken.js
@@ -0,0 +1,15 @@
+function breuken() {
+
+    let input1 = document.getElementById('getal1').value;
+    let getal1 = parseFloat(input1)
+    let uitkomst;
+    document.getElementById('uitkomst').innerHTML = "";
+
+    let counter = 0;
+
+
+
+
+    while (counter < 7) {
+        (counter++)
+    }
