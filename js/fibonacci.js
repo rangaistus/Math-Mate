18f87a70f0f607630f1e9be991c245107aab0ac3
rangaistus <6002029@mborijnland.nl>
2022-04-05 11:24:38 +0200

    Home page helemaal aangepast, css veranderd, javascript verbeterd van paar opdrachten.

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,13 +1,11 @@
-var i, fib=new Array(20);
+// fibonacci
 
-	var n=prompt("type de getal");
-	document.write("fibonacci numbers <br/>");
-	
-	fib[0]=1;
-	fib[1]=1;
-	document.write(fib[0],"<br/>",fib[1],"<br/>");
+function fibonacci(){
+    getal1 = document.getElementById("getal1").value;
+    let fibonacci = [0, 1];
 
-	for(i=0;i<n-2;i++) {
-		fib[i+2]=fib[i+1]+fib[i];
-		document.write(fib[i+2],"<br/>");
-	}
+    for (let i = 2; i < getal1; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    document.getElementById('uitkomst').innerHTML = fibonacci;
+} document.getElementById('bereken').addEventListener('click', fibonacci);
--- js/priemgetallen.js
+++ js/priemgetallen.js
@@ -1,22 +1,18 @@
 var num, i, chk, temp;
-function checkPrime()
-{
+function checkPrime(){
   num = parseInt(document.getElementById("num").value);
-  if(num)
-  {
+  if(num){
     chk=0;
     temp = document.getElementById("resPara");
     temp.style.display = "block";
-    for(i=2; i<num; i++)
-    {
-      if(num%2==0)
-      {
+    for(i=2; i<num; i++){
+      if(num%2==0){
         chk++;
         break;
       }
     }
     if(chk==0)
-      document.getElementById("res").innerHTML = "wel priemgetal";
+      document.getElementById("res").innerHTML = "wel een priemgetal";
     else
       document.getElementById("res").innerHTML = "geen priemgetal";
   }
